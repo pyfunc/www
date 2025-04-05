56b2b240ae8460753507bd37c37eaeef7e1fec63
Tom Softreck <info@softreck.dev>
2025-04-05 19:45:40 +0200

    Release version 0.1.3

--- js/config.js
+++ js/config.js
@@ -15,25 +15,44 @@ const PYFUNC_CONFIG = {
     // Repository Configuration
     repositories: [
         {
-            name: 'memfs',
-            description: 'Virtual in-memory file system module compatible with OS operations.',
-            topics: ['filesystem', 'storage', 'shell'],
-            language: 'Python',
-            url: 'https://github.com/pyfunc/memfs'
+            "name": "memfs",
+            "description": "Moduł memfs implementuje wirtualny system plików w pamięci. Ten moduł zapewnia interfejs zgodny z modułem os i zapewnia operacje na plikach i katalogach przechowywanych w pamięci RAM, a nie na dysku.",
+            "topics": ["filesystem", "storage", "shell", "cli", "memfs", "virual"],
+            "language": "Python",
+            "url": "https://github.com/pyfunc/memfs",
+            "license": "Apache License 2.0"
         },
         {
-            name: 'PySimpleGUI',
-            description: 'Create cross-platform GUI applications with incredible simplicity.',
-            topics: ['gui', 'desktop', 'cross-platform'],
-            language: 'Python',
-            url: 'https://github.com/pyfunc/PySimpleGUI'
+            "name": "PySimpleGUI",
+            "description": "Launched in 2018. It's 2023 and PySimpleGUI is actively developed & supported. Create complex windows simply. Supports tkinter, Qt, WxPython, Remi (in browser). Create GUI applications trivially with a full set of widgets. Multi-Window applications are also simple. 3.4 to 3.11 supported. 325+ Demo programs & Cookbook for rapid start. Extensive docs",
+            "topics": ["gui"],
+            "language": "Python",
+            "url": "https://github.com/pyfunc/PySimpleGUI",
+            "license": "GNU Lesser General Public License v3.0"
         },
         {
-            name: 'pykeepass',
-            description: 'Seamless interaction with KeePass databases (KDBX3 and KDBX4).',
-            topics: ['security', 'password-manager'],
-            language: 'Python',
-            url: 'https://github.com/pyfunc/pykeepass'
+            "name": "pykeepass",
+            "description": "Python library to interact with keepass databases (supports KDBX3 and KDBX4)",
+            "topics": ["security", "password-manager"],
+            "language": "Python",
+            "url": "https://github.com/pyfunc/pykeepass",
+            "license": "GNU General Public License v3.0"
+        },
+        {
+            "name": "passhole",
+            "description": "A secure hole for your passwords (KeePass CLI)",
+            "topics": ["security", "password-manager", "cli"],
+            "language": "Python",
+            "url": "https://github.com/pyfunc/passhole",
+            "license": "GNU General Public License v3.0"
+        },
+        {
+            "name": "lib",
+            "description": "all libs for cameramonit, cfo, ... projects",
+            "topics": ["docker", "ocr", "templates", "pydock", "pyfunc"],
+            "language": "Python",
+            "url": "https://github.com/pyfunc/lib",
+            "license": "Apache License 2.0"
         }
     ],
 
